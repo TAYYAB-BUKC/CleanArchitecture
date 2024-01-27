28124a9e7755160f3dc0d8ea247fa7e39fd0f55c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-01-28 03:22:14 +0500

    Successfully bind the teams grid data by following the Clean Architecture.

--- CleanArchitecture/CleanArchitecture.UI/Controllers/TeamController.cs
+++ CleanArchitecture/CleanArchitecture.UI/Controllers/TeamController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CleanArchitecture.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchitecture.UI.Controllers
 {
     public class TeamController : Controller
     {
+        public ITeamService TeamService { get; set; }
+        public TeamController(ITeamService teamService)
+        {
+            TeamService = teamService;
+        }
         public IActionResult Index()
         {
+            ViewBag.Teams = TeamService.GetAllTeams();
             return View();
         }
     }
--- CleanArchitecture/CleanArchitecture.UI/Program.cs
+++ CleanArchitecture/CleanArchitecture.UI/Program.cs
@@ -1,8 +1,13 @@
+using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Core.Services;
+using CleanArchitecture.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<ITeamService, TeamService>();
+builder.Services.AddScoped<ITeamRepository, TeamRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
